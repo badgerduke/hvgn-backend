d913d9baac06ce9761b58c3dd770cf0c52c0c70d
Eric Hamacher <erichamacher@protonmail.com>
2022-03-23 19:10:00 -0500

    removed uncesary files and add functionerrors alarm/sns

--- deploy/delete-api-keys.sh
+++
@@ -1,8 +0,0 @@
-API_KEY_IDS=$(aws apigateway get-api-keys | jq -r --arg stage $1 '.items[] | select(.name | startswith("hvgn-backend-\($stage)")).id')
-for API_KEY_ID in $API_KEY_IDS
-do
-    echo $API_KEY_ID
-    aws apigateway delete-api-key --api-key $API_KEY_ID
-done
-#ANY_REM=$(aws apigateway get-api-keys | jq -r '.items[].name')
-#echo "XXXXX" $ANY_REM "VVVV"
